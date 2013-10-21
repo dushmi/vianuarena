ad4c566ba67e61e069c40697c6cda3cb7d4a9eb7
dushmi <dushmi94@gmail.com>
2013-10-21 17:35:14 +0300

    wtf

--- www/controllers/penalty_edit.php
+++ www/controllers/penalty_edit.php
@@ -33,7 +33,7 @@ function controller_penalty_solve($user_id, $round_id) {
 
     $view = array();
     $view['title'] = 'Penalty Edit';
-    #$view['total_score'] = $total_score;
+    $view['total_score'] = $total_score;
     #$view['tasks'] = array();
 
     #foreach ($scores as $task_id)
