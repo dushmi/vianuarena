ff8f9206dca1c47959bc420874b5c221511b1629
dushmi <dushmi94@gmail.com>
2013-10-21 17:08:56 +0300

    WTF

--- common/db/score.php
+++ common/db/score.php
@@ -509,7 +509,8 @@ function total_score_get_by_user_id_and_round_id($user_id, $round_id) {
     $query = sprintf("SELECT * FROM 'ia_score_user_round'
                       WHERE 'user_id' = %s AND 'round_id' LIKE %s",
                       db_escape($user_id), db_quote($round_id));
+    echo("WTF");
     $total_score = db_fetch($query);
-    echo("WTF".$total_score);
+    echo("WTF");
     return $total_score;
 }
