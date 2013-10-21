2e6c299d1a68ffce9bb91d67b3980d4bcfefa676
dushmi <dushmi94@gmail.com>
2013-10-21 17:47:34 +0300

    Almost Done

--- www/views/penalty_edit.php
+++ www/views/penalty_edit.php
@@ -7,6 +7,10 @@
 <p>Modifica punctajele per problema.</p>
 
 <?php echo $view['total_score']; ?>
+<?php
+    foreach ($view['tasks'] as $task)
+        echo $task['task_id']." ".$task['score']."      ";
+    ?>
 
 <?php
     include('footer.php');
