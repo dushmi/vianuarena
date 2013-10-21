1fd34136c45a25cdf25ef71ad01067ef4c11880d
dushmi <dushmi94@gmail.com>
2013-10-21 14:02:00 +0300

    Penalty Controller V1

---
+++ www/controllers/penalty.php
@@ -0,0 +1,75 @@
+<?php
+
+require_once(IA_ROOT_DIR."common/db/smf.php");
+require_once(IA_ROOT_DIR."common/db/user.php");
+require_once(IA_ROOT_DIR."common/user.php");
+require_once(IA_ROOT_DIR."common/email.php");
+
+// displays form to identify user. On submit it sends e-mail with confirmation
+// link.
+
+function controller_penalty() {
+    global $identity_user;
+
+    // `data` dictionary is a dictionary with data to be displayed by form view
+    $data = array();
+
+    // here we store validation errors.
+    // It is a dictionary, indexed by field names
+    $errors = array();
+    
+    $changer_name = getattr($identity_user, 'username');
+    $changer = user_get_by_username($changer_name);
+    
+    if (!user_is_admin($changer))
+        redirect(url_home());
+
+    // submit?
+    $submit = request_is_post();
+
+    if ($submit) {
+        // 1. validate
+
+        $data['username'] = getattr($_POST, 'username');
+        // check username
+        if ($data['username']) {
+            $user = user_get_by_username($data['username']);
+            if (!$user) {
+                $errors['username'] = 'Nu exista vreun utilizator cu acest '
+                                      .'nume de cont';
+            }
+        }
+
+        $data['contest_id'] = getattr($_POST, 'contest_id');
+        // check contest_id
+        if ($data['contest_id']) {
+            $contest_id = round_get($data['contest_id']);
+            if (!$contest_id) {
+                $errors['contest_id'] = 'Nu exista runda cu acest '.'id';
+            }
+        }
+
+        #if (isset($user) && $user && !empty($new_pass)) {
+            // user was found
+        #    $user['password'] = user_hash_password($new_pass, $user['username']);
+        #    user_update($user);
+        #    redirect(url_home());
+        #}
+        #else {
+        #    flash_error('Trebuie sa completezi ambele campuri!');
+        #}
+    }
+    else {
+        // initial display of form
+    }
+
+    // page title
+    $view = array();
+    $view['title'] = 'Penalty';
+    $view['form_errors'] = $errors;
+    $view['form_values'] = $data;
+    $view['no_sidebar_login'] = true;
+    execute_view_die('views/penalty.php', $view);
+}
+
+?>
