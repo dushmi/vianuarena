451fc895e7c32225974c2247279218d3b2d73277
dushmi <dushmi94@gmail.com>
2013-10-21 15:45:15 +0300

    Pui de Koala

--- www/views/penalty_edit.php
+++ www/views/penalty_edit.php
@@ -9,26 +9,13 @@
 <form action="<?= html_escape(url_penalty_edit()) ?>" method="post" class="login clear">
 <fieldset>
     <legend>Probleme</legend>
-    <ul class="form">
-        <li>
-            <label for="form_username">Nume utilizator</label>
-            <?= ferr_span('username') ?>
-            <input type="text" name="username" id="form_username" value="<?= fval('username') ?>" />
-        </li>
-        
-        <li>
-            <label for="form_round_id">Id concurs</label>
-            <?= ferr_span('round_id') ?>
-            <input type="text" name="round_id" id="form_round_id" value="<?= fval('round_id') ?>" />
-        </li>
-
-    </ul>
+    
+    <?php
+    foreach ($view as $task_id)
+        echo $task_id." ".$view[$task_id];
+    ?>
 </fieldset>
-<ul class="form clear">
-    <li>
-        <input type="submit" value="Submit" id="form_submit" class="button important" />
-    </li>
-</ul>
+
 </form>
 
 <?php
