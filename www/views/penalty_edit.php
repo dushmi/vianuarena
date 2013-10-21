1a4932e77f7f63288d819873102958ffc77cfbe4
dushmi <dushmi94@gmail.com>
2013-10-21 18:03:48 +0300

    UI

--- www/views/penalty_edit.php
+++ www/views/penalty_edit.php
@@ -5,7 +5,7 @@
 <h1><?= html_escape($title) ?></h1>
 
 <?php 
-echo "Modifica punctajele per problema pentru utilizatorul <b>".$view['user']['username']."</b> la concursul <b>".$view['contest']['title']."</b>.";
+echo "Modifica punctajele per problema pentru utilizatorul <b>".$view['user']['username']."</b> la concursul <b>".$view['round']['title']."</b>.";
 echo "<h2>Scor total: ".$view['total_score']."</h2>"; 
 
 echo '<form action="<?= html_escape(url_penalty()) ?>" method="post" class="login clear">';
