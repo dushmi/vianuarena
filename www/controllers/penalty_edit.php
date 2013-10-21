d06b045f4e6a08854d3a9b1c5c652f7b9ff19c30
dushmi <dushmi94@gmail.com>
2013-10-21 16:18:25 +0300

    Bugged lines x3

--- www/controllers/penalty_edit.php
+++ www/controllers/penalty_edit.php
@@ -26,8 +26,8 @@ function controller_penalty_edit() {
 }
 
 function controller_penalty_solve($user_id, $round_id) {
-    $scores = scores_get_by_user_id_and_round_id($user_id, $round_id);
-    $total_score = total_score_get_by_user_id_and_round_id($user_id, $round_id);
+    #$scores = scores_get_by_user_id_and_round_id($user_id, $round_id);
+    #$total_score = total_score_get_by_user_id_and_round_id($user_id, $round_id);
 
     $view = array();
     $view['title'] = 'Penalty Edit';
