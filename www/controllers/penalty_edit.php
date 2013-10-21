05f059c04104ad26c8eb58f1e321baeab60d72cc
dushmi <dushmi94@gmail.com>
2013-10-21 14:50:31 +0300

    Penalty Page 2 Test

--- www/controllers/penalty_edit.php
+++ www/controllers/penalty_edit.php
@@ -8,8 +8,15 @@ require_once(IA_ROOT_DIR."common/email.php");
 // displays form to identify user. On submit it sends e-mail with confirmation
 // link.
 
-function controller_penalty_edit() {
+function controller_penalty_edit($user_id, $round_id) {
     global $identity_user;
+
+    //security check
+    $changer_name = getattr($identity_user, 'username');
+    $changer = user_get_by_username($changer_name);
+    
+    if (!user_is_admin($changer))
+        redirect(url_home());
 
     // `data` dictionary is a dictionary with data to be displayed by form view
     $data = array();
@@ -17,12 +24,7 @@ function controller_penalty_edit() {
     // here we store validation errors.
     // It is a dictionary, indexed by field names
     $errors = array();
-    
-    $changer_name = getattr($identity_user, 'username');
-    $changer = user_get_by_username($changer_name);
-    
-    if (!user_is_admin($changer))
-        redirect(url_home());
+    print ($user_id + ' ' + $round_id) 
 
     // submit?
     $submit = request_is_post();
@@ -37,7 +39,7 @@ function controller_penalty_edit() {
 
     // page title
     $view = array();
-    $view['title'] = 'Penalty';
+    $view['title'] = 'Penalty Edit';
     $view['form_errors'] = $errors;
     $view['form_values'] = $data;
     $view['no_sidebar_login'] = true;
