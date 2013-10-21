9e1f324000511dbc09c37f2d2a729c90855b53a9
dushmi <dushmi94@gmail.com>
2013-10-21 17:58:16 +0300

    UI

--- www/views/penalty_edit.php
+++ www/views/penalty_edit.php
@@ -7,6 +7,7 @@
 <p>Modifica punctajele per problema pentru utilizatorul</p>
 
 <?php 
+echo "Modifica punctajele per problema pentru utilizatorul ".$view['user'];
 echo "<h2>Scor total: ".$view['total_score']."</h2>"; 
 
 echo '<form action="<?= html_escape(url_penalty()) ?>" method="post" class="login clear">';
